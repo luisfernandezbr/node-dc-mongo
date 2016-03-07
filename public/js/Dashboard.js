8fd353ff1bbe3efbd89b7c03637a06306c03e16a
Luis Fernandez <luisfernandez@el-capitan-empresa-ssd-256.local>
2016-03-07 13:46:11 -0300

    changes to test imalive

--- public/js/Dashboard.js
+++ public/js/Dashboard.js
@@ -4,7 +4,7 @@ queue()
 
 function makeGraphs(error, apiData) {
 	
-//Start Transformations
+	//Start Transformations
 	var dataSet = apiData;
 	var dateFormat = d3.time.format("%m/%d/%Y");
 	dataSet.forEach(function(d) {
@@ -17,22 +17,15 @@ function makeGraphs(error, apiData) {
 	var ndx = crossfilter(dataSet);
 
 	//Define Dimensions
-	var datePosted = ndx.dimension(function(d) { return d.date_posted; });
-	var gradeLevel = ndx.dimension(function(d) { return d.grade_level; });
-	var resourceType = ndx.dimension(function(d) { return d.resource_type; });
-	var fundingStatus = ndx.dimension(function(d) { return d.funding_status; });
-	var povertyLevel = ndx.dimension(function(d) { return d.poverty_level; });
-	var state = ndx.dimension(function(d) { return d.school_state; });
-	var totalDonations  = ndx.dimension(function(d) { return d.total_donations; });
+	var datePosted = ndx.dimension(function(d) { return d.date_save; });
+	var versionCode = ndx.dimension(function(d) { return d.version_code; });
+	var frontend = ndx.dimension(function(d) { return d.frontend; });
 
 
 	//Calculate metrics
 	var projectsByDate = datePosted.group(); 
-	var projectsByGrade = gradeLevel.group(); 
-	var projectsByResourceType = resourceType.group();
-	var projectsByFundingStatus = fundingStatus.group();
-	var projectsByPovertyLevel = povertyLevel.group();
-	var stateGroup = state.group();
+	var projectsByVersionCode = versionCode.group();
+	var projectsByFrontend = frontend.group();
 
 	var all = ndx.groupAll();
 
@@ -41,39 +34,33 @@ function makeGraphs(error, apiData) {
 		return d.total_donations;
 	});
 
-	var totalDonationsGrade = gradeLevel.group().reduceSum(function(d) {
-		return d.grade_level;
+	var totalVersionCode = versionCode.group().reduceSum(function(d) {
+		return d.versionCode;
 	});
 
-	var totalDonationsFundingStatus = fundingStatus.group().reduceSum(function(d) {
-		return d.funding_status;
+	var totalFrontend = frontend.group().reduceSum(function(d) {
+		return d.frontend;
 	});
 
 
 
-	var netTotalDonations = ndx.groupAll().reduceSum(function(d) {return d.total_donations;});
+	//var netTotalDonations = ndx.groupAll().reduceSum(function(d) {return d.total_donations;});
 
 	//Define threshold values for data
-	var minDate = datePosted.bottom(1)[0].date_posted;
-	var maxDate = datePosted.top(1)[0].date_posted;
+	var minDate = datePosted.bottom(1)[0].date_save;
+	var maxDate = datePosted.top(1)[0].date_save;
 
-console.log(minDate);
-console.log(maxDate);
+	console.log(minDate);
+	console.log(maxDate);
 
     //Charts
 	var dateChart = dc.lineChart("#date-chart");
 	var gradeLevelChart = dc.rowChart("#grade-chart");
-	var resourceTypeChart = dc.rowChart("#resource-chart");
 	var fundingStatusChart = dc.pieChart("#funding-chart");
-	var povertyLevelChart = dc.rowChart("#poverty-chart");
-	var totalProjects = dc.numberDisplay("#total-projects");
-	var netDonations = dc.numberDisplay("#net-donations");
-	var stateDonations = dc.barChart("#state-donations");
 
-
-  selectField = dc.selectMenu('#menuselect')
-        .dimension(state)
-        .group(stateGroup); 
+  	selectField = dc.selectMenu('#menuselect')
+        .dimension(androidSdk)
+        .group(byAndroidSdk);
 
        dc.dataCount("#row-selection")
         .dimension(ndx)
@@ -85,18 +72,13 @@ console.log(maxDate);
 		.valueAccessor(function(d){return d; })
 		.group(all);
 
-	netDonations
-		.formatNumber(d3.format("d"))
-		.valueAccessor(function(d){return d; })
-		.group(netTotalDonations)
-		.formatNumber(d3.format(".3s"));
 
 	dateChart
 		//.width(600)
 		.height(220)
 		.margins({top: 10, right: 50, bottom: 30, left: 50})
 		.dimension(datePosted)
-		.group(projectsByDate)
+		.group(versionCode)
 		.renderArea(true)
 		.transitionDuration(500)
 		.x(d3.time.scale().domain([minDate, maxDate]))
@@ -106,26 +88,11 @@ console.log(maxDate);
 		.xAxisLabel("Year")
 		.yAxis().ticks(6);
 
-	resourceTypeChart
-        //.width(300)
-        .height(220)
-        .dimension(resourceType)
-        .group(projectsByResourceType)
-        .elasticX(true)
-        .xAxis().ticks(5);
-
-	povertyLevelChart
-		//.width(300)
-		.height(220)
-        .dimension(povertyLevel)
-        .group(projectsByPovertyLevel)
-        .xAxis().ticks(4);
-
 	gradeLevelChart
 		//.width(300)
 		.height(220)
-        .dimension(gradeLevel)
-        .group(projectsByGrade)
+        .dimension(frontend)
+        .group(projectsByFrontend)
         .xAxis().ticks(4);
 
   
@@ -135,32 +102,8 @@ console.log(maxDate);
             .radius(90)
             .innerRadius(40)
             .transitionDuration(1000)
-            .dimension(fundingStatus)
-            .group(projectsByFundingStatus);
-
-
-    stateDonations
-    	//.width(800)
-        .height(220)
-        .transitionDuration(1000)
-        .dimension(state)
-        .group(totalDonationsState)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
-        .centerBar(false)
-        .gap(5)
-        .elasticY(true)
-        .x(d3.scale.ordinal().domain(state))
-        .xUnits(dc.units.ordinal)
-        .renderHorizontalGridLines(true)
-        .renderVerticalGridLines(true)
-        .ordering(function(d){return d.value;})
-        .yAxis().tickFormat(d3.format("s"));
-
-
-
-
-
+            .dimension(versionCode)
+            .group(projectsByVersionCode);
 
     dc.renderAll();
-
 };
